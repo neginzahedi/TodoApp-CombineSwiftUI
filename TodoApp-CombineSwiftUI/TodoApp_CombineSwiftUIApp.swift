1e037bdf28bb62cf22355f0b0c3199691595c902
Negin Zahedi <zahedi.negin@gmail.com>
2024-06-18 12:31:22 -0400

    Initial Commit

---
+++ TodoApp-CombineSwiftUI/TodoApp_CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_CombineSwiftUIApp.swift
+//  TodoApp-CombineSwiftUI
+//
+//  Created by Negin Zahedi on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
